0b8005df80ea90d5a3065016f74bc34bb2abfa51
Zack-PC\Zack <vruizs01@gmail.com>
2016-09-29 20:29:17 +0200

    Añadido Servlet CicloVida

---
+++ src/java/es/albarregas/servlets/CicloVida.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package es.albarregas.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Zack
+ */
+@WebServlet(name = "CicloVida", urlPatterns = {"/ciclo"})
+public class CicloVida extends HttpServlet {
+
+    
+    @Override
+    public void init(ServletConfig config){
+        System.out.println("init()");
+}
+    
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        System.out.println("service()");
+    }
+    
+    @Override
+    public void destroy(){
+        System.out.println("destroy()");
+    }
+}
+
+    
